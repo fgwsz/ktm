ebecf06f9a9608c7222be18e558964292bb740b4
fgwsz <3191000458@qq.com>
2023-12-14 23:31:49 +0800

    增加git_push.ps1中途退出功能

--- git_push.ps1
+++ git_push.ps1
@@ -1,11 +1,16 @@
-git add include/*
-git add src/*
-git add CMakeLists.txt
-git add ktm.exe
-git add README.md
-git add msvc_build.ps1
-git add mingw_build.ps1
-git add git_push.ps1
+echo "you can input exit to abort git push."
 $commit_info=Read-Host -Prompt "input commit info"
-git commit -m $commit_info
-git push
+if($commit_info -eq "exit"){
+    echo "git push exit!"
+}else{
+    git add include/*
+    git add src/*
+    git add CMakeLists.txt
+    git add ktm.exe
+    git add README.md
+    git add msvc_build.ps1
+    git add mingw_build.ps1
+    git add git_push.ps1
+    git commit -m $commit_info
+    git push
+}
