c3a17becf45b0064d10fc0c65549a17e11b51e94
fgwsz <3191000458@qq.com>
2024-01-14 16:35:49 +0800

    clean code

--- mingw_build.ps1
+++
@@ -1,9 +0,0 @@
-if(Test-Path -Path mingw_build){}else{
-    mkdir mingw_build
-}
-cd mingw_build
-cmake -G"MinGW Makefiles" ..
-cmake --build . --config Release
-cd ..
-cp mingw_build/ktm.exe "ktm-mingw_build.exe"
-./"ktm-mingw_build.exe"
--- msvc_build.ps1
+++
@@ -1,9 +0,0 @@
-if(Test-Path -Path msvc_build){}else{
-    mkdir msvc_build
-}
-cd msvc_build
-cmake -G"Visual Studio 17 2022" ..
-cmake --build . --config Release
-cd ..
-cp msvc_build/Release/ktm.exe "ktm-msvc_build.exe"
-./"ktm-msvc_build.exe"
